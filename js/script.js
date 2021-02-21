5c071207f79c7ace463b7778efcfe3e4fcb05c05
Stanislav <eustce442@gmail.com>
2021-02-21 14:31:01 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+" use strict";
+
+/**const category = 'toys';
+console.log( `https.//someurl.com/${category}/5`); 
+const user = 'aloe';
+
+alert(`hi,${user}`);*/
+
+// let incr = 10,
+//     decr = 10;
+
+// //++incr;
+//--decr;
+//console.log(++incr);
+//console.log(--decr);
+
+console.log(2*4=='8');
+
+const isCheked = true,
+isClose = false;
+
+console.log(isCheked || !isClose);
