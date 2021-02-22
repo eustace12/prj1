17bdd5e9e7739ff249286fa2280f7812121c74c3
Stanislav <eustce442@gmail.com>
2021-02-22 10:55:18 +0200

    1practice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 " use strict";
+const NumberOfFilms = +prompt('How many movies have you seen??')
+
+const moviebd = {
+    count: NumberOfFilms,
+    movies: {},//пустой объект 
+    actors: {},
+    geners: [],
+    ptivat: false
+};
+
+
+const a = prompt( "Послед фильм?",""),
+      b = prompt("dfdf?", ""),
+      c = prompt("dfsssdf?", ""),
+      d = prompt("dfdffffff?", "");
+
+  moviebd.movies[a]=b; 
+  moviebd.movies[c]=d; 
+  
+  console.log(moviebd);
