0f469305138c1e96bc2e9c0d5a9a397b55e49a4f
Stanislav <eustce442@gmail.com>
2021-02-25 12:52:57 +0200

    method

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strcit";
+
+const str = "trst";
+
+// console.log(str[2] = "d");
+
+console.log(str.toUpperCase());
+console.log(str);
+
+let fruit = "some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "hell gell ";
+
+// console.log(logg.slice(5,9));
+
+// console.log(logg.substring(5,9));
+
+// console.log(logg.substr(5,4));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2";
+// console.log(parseInt(test));
+console.log(parseFloat(test));
